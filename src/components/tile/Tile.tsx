477e1708065f2651480550bf6691e8a72b1aff55
Pablo Vasquez <vascor.pablo@gmail.com>
2025-08-21 14:25:59 -0400

    tailwind configured correctly

--- src/components/tile/Tile.tsx
+++ src/components/tile/Tile.tsx
@@ -1,11 +1,14 @@
-import type { ITile } from '../../types/ITile';
-import type { FC } from 'react';
+const Tile = () => {
 
-const Tile: FC<ITile> = ({ value , points }) => {
+  const value = Math.round(Math.random()*10);
+  const points: number = 1;
+
+
   return (
-    <div className="tile">
-      <span>{value}</span>
-      <span>{points}</span>
+    <div className="tile font-bold text-2xl flex items-center justify-center bg-gray-200 rounded-lg shadow-md p-4">
+      <span className="text-gray-800">{value}</span>
+
+      <span className="text-gray-600">{points}</span>
     </div>
   )
 }
